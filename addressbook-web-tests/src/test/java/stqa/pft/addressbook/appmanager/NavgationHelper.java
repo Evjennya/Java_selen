588656b2971cd3427961283735da1d1a78ba60c2
Evgenya Prokhorova <ep@voltmobi.com>
2018-07-11 15:57:08 +0300

    Оптимизированы переходы между страницами

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/appmanager/NavgationHelper.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/appmanager/NavgationHelper.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by prohorova on 09.06.2018.
  */
-public class NavgationHelper extends HelperBase{
+public class NavgationHelper extends HelperBase {
 
 
   public NavgationHelper(WebDriver wd) {
@@ -16,8 +16,18 @@ public class NavgationHelper extends HelperBase{
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    if (isElementPresent(By.tagName("h1")) && wd.findElement(By.tagName("h1")).getText().equals("Groups") && isElementPresent(By.name("new"))) {
+      return;
+
+    }
+      click(By.linkText("groups"));
+
   }
 
-  public void gotoHomePage() {click(By.linkText("home"));}
+  public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
+    click(By.linkText("home"));
+  }
 }
