95d20f5c90fb8cfe53ed302d9b6952c8d939b9fb
Evgenya Prokhorova <ep@voltmobi.com>
2018-07-11 16:28:00 +0300

    Реализована проверка и обеспечивание выполнения предусловия

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import stqa.pft.addressbook.model.GroupData;
 
 
 public class GroupDeletionTests extends TestBase {
@@ -9,6 +10,10 @@ public class GroupDeletionTests extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavgationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deletSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
