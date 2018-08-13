e3fb2e49835b6d5d4a27ce1414b6407bc8901849
Evgenya Prokhorova <ep@voltmobi.com>
2018-08-13 15:58:19 +0300

    Повсеместное использование уникальных идентификаторов объектов

--- addressbook-web-tests/src/test/java/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/stqa/pft/addressbook/model/GroupData.java
@@ -1,6 +1,23 @@
 package stqa.pft.addressbook.model;
 
 public class GroupData {
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    GroupData groupData = (GroupData) o;
+
+    if (id != groupData.id) return false;
+    return name != null ? name.equals(groupData.name) : groupData.name == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id;
+    result = 31 * result + (name != null ? name.hashCode() : 0);
+    return result;
+  }
 
   private int id = Integer.MAX_VALUE;
   private String name;
@@ -50,21 +67,6 @@ public class GroupData {
             "id='" + id + '\'' +
             ", name='" + name + '\'' +
             '}';
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-
-    GroupData groupData = (GroupData) o;
-
-    return name != null ? name.equals(groupData.name) : groupData.name == null;
-  }
-
-  @Override
-  public int hashCode() {
-    return name != null ? name.hashCode() : 0;
   }
 
 }
